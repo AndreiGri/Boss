975449a2caa30ea97f57194fe3db6c657287836c
Andrei Gridin <andreigridin918@gmail.com>
2023-10-16 21:40:51 +0500

    Добавьте файлы проекта.

---
+++ Boss/Boss.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки терминала:
+    system("chcp 1251 > nul");
+    // Установка цвета фона терминала и цвета текста
+    system("color 1F");
+
+    cout << endl;
+    string answer;
+
+    do {
+        cout << " Вы выполнили задание?" << endl << " ";
+        getline(cin, answer);
+    } while (answer != "Да, конечно выполнил!");
+    cout << " Молодец!" << endl;
+
+
+    // Задержка окна консоли:
+    system("pause > nul");
+
+    return 0;
+}
